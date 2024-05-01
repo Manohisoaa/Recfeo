076f4e2fa816c32ddce3d22c25c902cdf156d09d
Manohisoaa <manohisoanirina707@mail.com>
2024-05-01 13:00:46 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <div className="w-full h-screen flex justify-center items-center">
+      <p className="text-2xl">Hello les fillllessss</p>
+    </div>
+  )
+}
+
+export default App
