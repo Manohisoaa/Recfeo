e0b6a3a6cdbb9535241e3f6cfc8b1bc1eeb743f2
Manohisoaa <manohisoanirina707@mail.com>
2024-05-06 18:14:20 +0300

    modification icon

--- src/react/Accueil.jsx
+++ src/react/Accueil.jsx
@@ -1,29 +1,34 @@
-import "../css/Accueil.css"
 import logo from "../assets/logo.png"
 import { useNavigate } from "react-router-dom"
+import { Button } from "../components/Bouton";
 
 
 function Accueil() {
 
-  const navigate=useNavigate();
-  
-  const Importer = () =>{
+  const navigate = useNavigate();
+
+  const Importer = () => {
     console.log('Mety');
   }
-  const Chanter = () =>{
+  const Chanter = () => {
     navigate("/chanter")
   }
 
   return (
-   <div className="fond bg-cover h-screen  ">
-    <div className="flex flex-col items-center justify-center h-screen  ">
-      <img src={logo} className="object-cover h-40 my-4 "/>
-      <h1 className="text-white text-2xl my-5">Rec'Feo</h1>
-      <button  onClick={Importer} className="bg-transparent h-16 w-60 rounded-full border-2 border-white m-30 text-white text-xl my-6 ">Importer</button>
-      <button  onClick={Chanter} className="bg-transparent h-16 w-60 rounded-full border-2 border-white m-30 text-white text-xl my-6 ">Chanter</button>
-    </div>
+    <div className=" bg-cover h-screen bg-[url('/back.png')] ">
+      <div className="flex flex-col items-center justify-center h-screen  ">
+        <img src={logo} className="object-cover h-40 my-4 " />
+        <h1 className="text-white text-2xl my-5">Rec Feo</h1>
+        {/* <Button>
+          Importer
+        </Button>
+ */}
 
-   </div>
+        <button onClick={Importer} className="bg-transparent h-16 w-60 rounded-full border-2 border-white m-30 text-white text-xl my-6 ">Importer</button>
+        <button onClick={Chanter} className="bg-transparent h-16 w-60 rounded-full border-2 border-white m-30 text-white text-xl my-6 ">Chanter</button>
+      </div>
+
+    </div>
   )
 }
 
--- src/react/Chanter.jsx
+++ src/react/Chanter.jsx
@@ -1,31 +1,35 @@
-import menu from "../assets/menu.svg"
-import logo from"../assets/logo.png"
-import micro from "../assets/microphone.svg"
-import save from "../assets/save.svg"
-import restart from "../assets/restart.svg"
-import casque from "../assets/casque.svg"
-import stop from "../assets/stop.svg"
-import supprimer from "../assets/delete.svg"
+
+import logo from "../assets/logo.png"
+
+import { CircleStop, Headphones, Import, Menu, Mic, RotateCcw, Trash2 } from "lucide-react"
 
 export default function Chanter() {
+
   return (
     <div className="bg-[#0A132D] h-screen fixed w-full ">
-        <div className="flex justify-between m-10">
-            <img src={logo} className="h-20"/>
-            <img src={menu} className="h-10"/>
+      <div className="flex justify-between m-10">
+        <img src={logo} className="h-20" />
+        <div>
+          <Menu color="white" size={40} />
+
         </div>
-        <div className="flex flex-col justify-evenly items-center  h-5/6  ">
-            <img src={micro} className="h-52 " />
-            <button className="bg-transparent h-14 w-52 bg-[#09153B] rounded-full border-4 border-white  text-white text-xl  ">Prêt</button>
-            <div className="flex">
-              <img src={save} className="px-20"/>
-              <img src={restart} className="px-20" />
-              <img src={casque} className="px-20"/>
-              <img src={stop} className="px-20"/>
-              <img src={supprimer} className="px-20"/>
-            </div>
+      </div>
+      <div className="flex flex-col gap-16 items-center  h-5/6  ">
+        <div className="p-10 bg-white/5 rounded-full border-white border-4">
+          <Mic size={100} color="white" />
         </div>
-        
-    </div> 
+        <button className="bg-transparent h-14 w-52 bg-[#09153B] rounded-full border-4 border-white  text-white text-xl  ">Prêt</button>
+        <div className="flex gap-40">
+
+          {/* eo amin io onClick io rehefa hanisy action amin ilay izy */}
+          <Import size={40} color="white" onClick={() => alert("Import")} />
+          <RotateCcw size={40} color="white" />
+          <Headphones size={40} color="white" />
+          <CircleStop size={40} color="white" />
+          <Trash2 size={40} color="white" />
+        </div>
+      </div>
+
+    </div>
   )
 }
